1ab20317c00a3c942aba3212f5f989f8979463a6
Caio Gomes <caiogomes34@gmail.com>
2022-04-07 19:45:33 -0300

    remove default react app stuff

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // TODO: define the render of the page here
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
