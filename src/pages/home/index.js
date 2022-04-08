469ac46de6e127de453eabe35dc72b65e5b4b271
Caio Gomes <caiogomes34@gmail.com>
2022-04-08 06:50:46 -0300

    setup the size of the elements properly

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom"
 import styles from './styles.module.css'
 import buttonStyles from '../../components/buttons.module.css'
+import headerStyles from '../../components/headers.module.css'
 
 const Home = () => {
   return <section className={styles.app}>
     <div className={styles.floatArea}>
-      <h1>Productive Mind</h1>
+      <div className={headerStyles.header}>
+        <h1>Productive Mind</h1>
+      </div>
       <span>Faça mais com menos</span>
       <div className={styles.buttonContainer}>
         <Link to={"/app"} className={buttonStyles.fullButton}>Seguir para o app</Link>
