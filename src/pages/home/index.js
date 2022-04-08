94b9817d25b7a5ceb277bf23c37ef827db2eb66b
Caio Gomes <caiogomes34@gmail.com>
2022-04-07 22:18:53 -0300

    setup the default interface management

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import styles from './styles.module.css'
+import buttonStyles from '../../components/buttons.module.css'
 
 const Home = () => {
   return <section className={styles.app}>
     <div className={styles.floatArea}>
       <h1>Productive Mind</h1>
       <span>Faça mais com menos</span>
-      <Link to={"/app"} />
+      <Link to={"/app"} className={buttonStyles.fullButton}>Seguir para o app</Link>
     </div>
   </section>
 }
