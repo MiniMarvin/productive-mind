e5efed5c4bc9e699943db2a48b8c56f48da9fba0
Caio Gomes <caiogomes34@gmail.com>
2022-04-08 08:55:05 -0300

    add task screen

--- src/pages/new-task/index.js
+++ src/pages/new-task/index.js
@@ -1,6 +1,57 @@
+import { useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles.module.css'
+import inputStyles from '../../components/inputs.module.css'
+import containerStyles from '../../components/containers.module.css'
+import buttonStyles from '../../components/buttons.module.css'
+import { categories } from '../../services/categoryService'
+
+const emptyString = (string) => {
+  return !(string !== null && string.replace(' ', '') !== '')
+}
+
 const NewTask = () => {
-  return <main>
-    NewTask
+  let [selectedCategoryIndex, setSelectedCategoryIndex] = useState(null)
+  const inputRef = useRef()
+
+  const renderButtons = () => {
+    return categories.map((category, idx) => {
+      const selectButton = () => {
+        if (selectedCategoryIndex !== idx) setSelectedCategoryIndex(idx)
+        else setSelectedCategoryIndex(null)
+      }
+
+      const buttonStyle = selectedCategoryIndex === idx ? buttonStyles.fullButton : buttonStyles.lineButton
+
+      return (
+        <div className={styles.buttonContainer} key={`category-button-${idx}`}>
+          <button className={buttonStyle} onClick={selectButton}>{category}</button>
+        </div>
+      )
+    })
+  }
+
+  const saveItem = () => {
+    const taskName = inputRef.current.value
+    console.log('task name: ', taskName)
+    console.log('category: ', categories[selectedCategoryIndex])
+  }
+
+  const isButtonEnabled = selectedCategoryIndex !== null && !emptyString(inputRef.current.value)
+
+  return <main className={containerStyles.page}>
+    <Link to="/add-tasks" className={buttonStyles.backButton}>{'<'} voltar</Link>
+    <div className={containerStyles.header}>
+      <h1>Nova Atividade</h1>
+    </div>
+    <span>descreva a atividade e sua categoria</span>
+    <input type="text" className={inputStyles.input} placeholder="nova atividade..." ref={inputRef}></input>
+    <section className={styles.buttons}>
+      {renderButtons()}
+    </section>
+    <section className={styles.save}>
+      <button className={buttonStyles.fullButton} onClick={saveItem} disabled={!isButtonEnabled}>salvar</button>
+    </section>
   </main>
 }
 
---
+++ src/services/categoryService.js
@@ -0,0 +1,5 @@
+export const categories = [
+  'carreira',
+  'pessoal',
+  'estudo'
+]
